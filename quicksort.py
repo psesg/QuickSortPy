248e08ba06cc5207b96f3d86555acf127f928155
Serg <pseesp@mail.ru>
2022-09-14 17:06:08 +0300

    Initial commit

---
+++ quicksort.py
@@ -0,0 +1,66 @@
+# Quick sort in Python
+
+import sys
+import datetime
+import random
+
+# function to find the partition position
+def partition(array, low, high):
+
+  # choose the rightmost element as pivot
+  pivot = array[high]
+
+  # pointer for greater element
+  i = low - 1
+
+  # traverse through all elements
+  # compare each element with pivot
+  for j in range(low, high):
+    if array[j] <= pivot:
+      # if element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+
+      # swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+
+  # swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+  # return the position from where partition is done
+  return i + 1
+
+# function to perform quicksort
+def quickSort(array, low, high):
+  if low < high:
+
+    # find pivot element such that
+    # element smaller than pivot are on the left
+    # element greater than pivot are on the right
+    pi = partition(array, low, high)
+
+    # recursive call on the left of pivot
+    quickSort(array, low, pi - 1)
+
+    # recursive call on the right of pivot
+    quickSort(array, pi + 1, high)
+
+
+inputlist = []
+ncount = int(sys.stdin.readline().strip("\n"))
+big_str = sys.stdin.readline().strip("\n")
+big_strips = big_str.split(' ')
+for i in range(ncount):
+    inputlist.append(int(big_strips[i]))
+
+print("Got unsorted Array")
+# print(inputlist)
+
+size = len(inputlist)
+a = datetime.datetime.now()
+quickSort(inputlist, 0, size - 1)
+b = datetime.datetime.now()
+c = b - a
+newsize = len(inputlist)
+print('Sorted int Array length = {} in {}: '.format(newsize, c))
+# print(inputlist)
